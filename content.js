8a555df4b865f17a39dc27dd13d36f63a22bf3c1
pjsier <pjsier@gmail.com>
2015-05-25 15:51:24 -0400

    use SoQL query

--- content.js
+++ content.js
@@ -1,16 +1,19 @@
 function requestData() {
-  var url = 'http://data.cityofchicago.org/resource/cwig-ma7x.json?';
+  var url = 'http://data.cityofchicago.org/resource/cwig-ma7x.json?$where=';
 
   // Set up an asynchronous AJAX POST request
   var xhr = new XMLHttpRequest();
 
   // Prepare the data to be POSTed by URLEncoding each field's contents
-  var resName = document.querySelector('.biz-page-title').innerText.toUpperCase().replace(/ /g,"+");
-  var resAddr = document.querySelector('.street-address').querySelector('span').innerText.toUpperCase().replace(/ /g,"+");
-  resName = resName.replace(/\&/g,"%26");
+  var resName = document.querySelector('.biz-page-title').innerText;
+  var resAddr = document.querySelector('.street-address').querySelector('span').innerText;
 
-  var params = 'aka_name=' + resName +
-               '&address=' + resAddr + '+';
+  //encodeURIComponent
+
+  var params = 'aka_name like ' + resName +
+               'AND address like ' + resAddr + ' ';
+
+  var queryString = encodeURIComponent(url+params);
 
   console.log(url+params);
 
@@ -36,10 +39,11 @@ function callback(food) {
   var results = food[fE]["results"];
   var violations = food[fE]["violations"].replace(/\|/g,"<br>");
 
-  document.querySelector('.showcase-container').innerHTML = "<div id='result-container'> <b>Name: </b>" +
-  resName + "<br><b>Address: </b>" + address + "<br><b>Date: </b>" + date +
-  "<br><b>Inspection Type: </b>" + iType + "<br><b>Results: </b>" + results +
-  "<br><b>Violations: </b>" + violations + "<div>";
+  document.querySelector('.showcase-container').innerHTML =
+    "<div id='result-container'> <b>Name: </b>" + resName +
+    "<br><b>Address: </b>" + address + "<br><b>Date: </b>" + date +
+    "<br><b>Inspection Type: </b>" + iType + "<br><b>Results: </b>" + results +
+    "<br><b>Violations: </b>" + violations + "</div>";
 }
 
 requestData();
